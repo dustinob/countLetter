7cd6f8b2a8960bd6249c834dbda315464f8898c4
Dustin OBrien <dcobrien82@gmail.com>
2017-01-11 20:11:07 +0000

    Function splits and joins with no spaces

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+// return all the unique charactors that are passed into the function
+
+// count how many of each character are returned
+
+// Returns an object of the stats for the sentence given {character: count}
+
+
+function countLetters(sentence) {
+
+  var splitSentence = sentence.split(" ").join("");
+  console.log(splitSentence);
+  //cycle thought each letter
+  for (var i = 0; i < splitSentence.length; i++) {
+
+    //console.log(i);
+  }
+
+}
+countLetters("Lighthouse in the house")
